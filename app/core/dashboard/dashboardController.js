dbcc7c07268a083358c25dff64c96e56ba35300b
Adebayo Adekunle <adebayo.adekunle006@gmail.com>
2018-05-06 23:38:02 +0100

    table and graph details done

--- app/core/dashboard/dashboardController.js
+++ app/core/dashboard/dashboardController.js
@@ -10,7 +10,7 @@
     function DashboardController($log) {
         var vm = this;
 
-        vm.options = {
+        vm.healthOptions = {
             chart: {
                 type: 'lineChart',
                 height: 250,
@@ -88,61 +88,170 @@
             }
         };
 
-        vm.data = sinAndCos();
+        vm.financialOptions = {
+            chart: {
+                type: 'lineChart',
+                height: 250,
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 30
+                },
+                x: function (d) {
+                    return d.x;
+                },
+                y: function (d) {
+                    return d.y;
+                },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function (e) {
+                        console.log("stateChange");
+                    },
+                    changeState: function (e) {
+                        console.log("changeState");
+                    },
+                    tooltipShow: function (e) {
+                        console.log("tooltipShow");
+                    },
+                    tooltipHide: function (e) {
+                        console.log("tooltipHide");
+                    }
+                },
+                xAxis: {
+                    axisLabel: 'Month',
+                    ticks: '12',
+                    showMaxMin: false,
+                    tickFormat: function (d,i) {
 
-        vm.dat = [];
+                        var weekday = ["Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"];
 
-        vm.dat.push(vm.data[0]);
+                        return weekday[i];
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Voltage (v)',
+                    tickFormat: function (d) {
+                        return d;
+                    },
+                    axisLabelDistance: -10,
+                    showMaxMin: false,
+                    tickPadding: 10
+                },
+                callback: function (chart) {
+                    console.log("!!! lineChart callback !!!");
+                },
+                showLegend: false
+            },
+            title: {
+                enable: false,
+                text: 'Title for Line Chart'
+            },
+            subtitle: {
+                enable: false,
+                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                css: {
+                    'text-align': 'center',
+                    'margin': '10px 13px 0px 7px'
+                }
+            },
+            caption: {
+                enable: false,
+                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+                css: {
+                    'text-align': 'justify',
+                    'margin': '10px 13px 0px 7px'
+                }
+            }
+        };
+
+        vm.healthData = healthData();
+
+        vm.financialData = financialData();
 
         /*Random Data Generator */
-        function sinAndCos() {
+        function healthData() {
             var sin = [],
                 sin2 = [],
                 cos = [],
-                cos2 = [];
+                cos2 = [],
+                cos3 = [];
 
             //Data is represented as an array of {x,y} pairs.
             for (var i = 0; i < 7; i++) {
                 sin.push({
                     x: i,
-                    y: Math.random() * 1000
+                    y: Math.floor(Math.random() * 1000)
                 });
                 sin2.push({
                     x: i,
-                    y: Math.random() * 1000
+                    y: Math.floor(Math.random() * 1000)
                 });
                 cos.push({
                     x: i,
-                    y: Math.random() * 1000
+                    y: Math.floor(Math.random() * 1000)
                 });
                 cos2.push({
                     x: i,
-                    y: Math.random() * 1000
+                    y: Math.floor(Math.random() * 1000)
+                });
+                cos3.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
                 });
             }
 
             //Line chart data should be sent as an array of series objects.
             return [{
                     values: sin, //values - represents the array of {x,y} data points
-                    key: 'Sine Wave', //key  - the name of the series.
-                    color: '#ff7f0e' //color - optional: choose your own line color.
+                    key: 'Malaria', //key  - the name of the series.
+                    color: '#DA80D1' //color - optional: choose your own line color.
                 },
                 {
                     values: cos,
-                    key: 'Cosine Wave',
-                    color: '#2ca02c'
+                    key: 'Lassa Fever',
+                    color: '#67B0C3'
                 },
                 {
                     values: sin2,
-                    key: 'Another sine wave',
-                    color: '#7777ff',
+                    key: 'Cholera',
+                    color: '#FFD876',
                     //area: true      //area - set to true if you want this line to turn into a filled area chart.
                 },
                 {
                     values: cos2,
-                    key: 'Another Cosine wave',
-                    color: '#cccccc',
+                    key: 'Thyphoid',
+                    color: '#B8FF2F',
                     //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                },
+                {
+                    values: cos3,
+                    key: 'Diabeties',
+                    color: '#73CBA7',
+                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                }
+            ];
+        };
+
+        /*Random Data Generator */
+        function financialData() {
+            var sin = [];
+
+            //Data is represented as an array of {x,y} pairs.
+            for (var i = 0; i < 12; i++) {
+                sin.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 100)
+                });
+            }
+
+            //Line chart data should be sent as an array of series objects.
+            return [{
+                    values: sin, //values - represents the array of {x,y} data points
+                    key: 'Sum Total (mill)', //key  - the name of the series.
+                    color: '#DA80D1', //color - optional: choose your own line color.
+                    face: 'hpojksn'
                 }
             ];
         };
--- app/core/epidemiological/trendsController.js
+++ app/core/epidemiological/trendsController.js
@@ -10,7 +10,85 @@
     function TrendsController($log) {
         var vm = this;
 
-        vm.options = {
+        vm.statusOptions = {
+            chart: {
+                type: 'lineChart',
+                height: 250,
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 45
+                },
+                x: function (d) {
+                    return d.x;
+                },
+                y: function (d) {
+                    return d.y;
+                },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function (e) {
+                        console.log("stateChange");
+                    },
+                    changeState: function (e) {
+                        console.log("changeState");
+                    },
+                    tooltipShow: function (e) {
+                        console.log("tooltipShow");
+                    },
+                    tooltipHide: function (e) {
+                        console.log("tooltipHide");
+                    }
+                },
+                xAxis: {
+                    axisLabel: '',
+                    ticks: '7',
+                    showMaxMin: false,
+                    tickFormat: function (d,i) {
+
+                        var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+                        return weekday[i];
+                    }
+                },
+                yAxis: {
+                    axisLabel: '',
+                    tickFormat: function (d) {
+                        return d;
+                    },
+                    axisLabelDistance: -10,
+                    showMaxMin: false,
+                    tickPadding: 10
+                },
+                callback: function (chart) {
+                    console.log("!!! lineChart callback !!!");
+                },
+                showLegend: false
+            },
+            title: {
+                enable: false,
+                text: 'Title for Line Chart'
+            },
+            subtitle: {
+                enable: false,
+                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                css: {
+                    'text-align': 'center',
+                    'margin': '10px 13px 0px 7px'
+                }
+            },
+            caption: {
+                enable: false,
+                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+                css: {
+                    'text-align': 'justify',
+                    'margin': '10px 13px 0px 7px'
+                }
+            }
+        };
+
+        vm.diseasesOptions = {
             chart: {
                 type: 'lineChart',
                 height: 250,
@@ -88,63 +166,367 @@
             }
         };
 
-        vm.data = sinAndCos();
+        vm.genderOptions = {
+            chart: {
+                type: 'lineChart',
+                height: 250,
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 30
+                },
+                x: function (d) {
+                    return d.x;
+                },
+                y: function (d) {
+                    return d.y;
+                },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function (e) {
+                        console.log("stateChange");
+                    },
+                    changeState: function (e) {
+                        console.log("changeState");
+                    },
+                    tooltipShow: function (e) {
+                        console.log("tooltipShow");
+                    },
+                    tooltipHide: function (e) {
+                        console.log("tooltipHide");
+                    }
+                },
+                xAxis: {
+                    axisLabel: 'Time (ms)',
+                    ticks: '7',
+                    showMaxMin: false,
+                    tickFormat: function (d,i) {
 
-        vm.dat = [];
+                        var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 
-        vm.dat.push(vm.data[0]);
+                        return weekday[i];
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Voltage (v)',
+                    tickFormat: function (d) {
+                        return d;
+                    },
+                    axisLabelDistance: -10,
+                    showMaxMin: false,
+                    tickPadding: 10
+                },
+                callback: function (chart) {
+                    console.log("!!! lineChart callback !!!");
+                },
+                showLegend: false
+            },
+            title: {
+                enable: false,
+                text: 'Title for Line Chart'
+            },
+            subtitle: {
+                enable: false,
+                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                css: {
+                    'text-align': 'center',
+                    'margin': '10px 13px 0px 7px'
+                }
+            },
+            caption: {
+                enable: false,
+                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+                css: {
+                    'text-align': 'justify',
+                    'margin': '10px 13px 0px 7px'
+                }
+            }
+        };
+
+        vm.ageOptions = {
+            chart: {
+                type: 'lineChart',
+                height: 250,
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 30
+                },
+                x: function (d) {
+                    return d.x;
+                },
+                y: function (d) {
+                    return d.y;
+                },
+                useInteractiveGuideline: true,
+                dispatch: {
+                    stateChange: function (e) {
+                        console.log("stateChange");
+                    },
+                    changeState: function (e) {
+                        console.log("changeState");
+                    },
+                    tooltipShow: function (e) {
+                        console.log("tooltipShow");
+                    },
+                    tooltipHide: function (e) {
+                        console.log("tooltipHide");
+                    }
+                },
+                xAxis: {
+                    axisLabel: 'Time (ms)',
+                    ticks: '7',
+                    showMaxMin: false,
+                    tickFormat: function (d,i) {
+
+                        var weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+                        return weekday[i];
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Voltage (v)',
+                    tickFormat: function (d) {
+                        return d;
+                    },
+                    axisLabelDistance: -10,
+                    showMaxMin: false,
+                    tickPadding: 10
+                },
+                callback: function (chart) {
+                    console.log("!!! lineChart callback !!!");
+                },
+                showLegend: false
+            },
+            title: {
+                enable: false,
+                text: 'Title for Line Chart'
+            },
+            subtitle: {
+                enable: false,
+                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                css: {
+                    'text-align': 'center',
+                    'margin': '10px 13px 0px 7px'
+                }
+            },
+            caption: {
+                enable: false,
+                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+                css: {
+                    'text-align': 'justify',
+                    'margin': '10px 13px 0px 7px'
+                }
+            }
+        };
+
+        vm.statusData = statusData();
+
+        vm.diseasesData = diseasesData();
+
+        vm.genderData = genderData();
+
+        vm.ageData = ageData();
 
         /*Random Data Generator */
-        function sinAndCos() {
+        function statusData() {
             var sin = [],
                 sin2 = [],
-                cos = [],
-                cos2 = [];
+                cos = [];
 
             //Data is represented as an array of {x,y} pairs.
             for (var i = 0; i < 7; i++) {
                 sin.push({
                     x: i,
-                    y: Math.random() * 1000
+                    y: Math.floor(Math.random() * 50000)
                 });
                 sin2.push({
                     x: i,
-                    y: Math.random() * 1000
+                    y: Math.floor(Math.random() * 50000)
                 });
                 cos.push({
                     x: i,
-                    y: Math.random() * 1000
-                });
-                cos2.push({
-                    x: i,
-                    y: Math.random() * 1000
+                    y: Math.floor(Math.random() * 10000)
                 });
             }
 
             //Line chart data should be sent as an array of series objects.
             return [{
                     values: sin, //values - represents the array of {x,y} data points
-                    key: 'Sine Wave', //key  - the name of the series.
-                    color: '#ff7f0e' //color - optional: choose your own line color.
+                    key: 'Critical', //key  - the name of the series.
+                    color: '#DA80D1' //color - optional: choose your own line color.
                 },
                 {
                     values: cos,
-                    key: 'Cosine Wave',
-                    color: '#2ca02c'
+                    key: 'Moderate',
+                    color: '#67B0C3'
                 },
                 {
                     values: sin2,
-                    key: 'Another sine wave',
-                    color: '#7777ff',
-                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
-                },
-                {
-                    values: cos2,
-                    key: 'Another Cosine wave',
-                    color: '#cccccc',
+                    key: 'Minor',
+                    color: '#FFD876',
                     //area: true      //area - set to true if you want this line to turn into a filled area chart.
                 }
             ];
         };
+
+        function diseasesData() {
+            var sin = [],
+                sin2 = [],
+                cos = [],
+                cos2 = [],
+                cos3 = [];
+
+            //Data is represented as an array of {x,y} pairs.
+            for (var i = 0; i < 7; i++) {
+                sin.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                sin2.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                cos.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                cos2.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                cos3.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+            }
+
+            //Line chart data should be sent as an array of series objects.
+            return [{
+                    values: sin, //values - represents the array of {x,y} data points
+                    key: 'Malaria', //key  - the name of the series.
+                    color: '#DA80D1' //color - optional: choose your own line color.
+                },
+                {
+                    values: cos,
+                    key: 'Lassa Fever',
+                    color: '#67B0C3'
+                },
+                {
+                    values: sin2,
+                    key: 'Cholera',
+                    color: '#FFD876',
+                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                },
+                {
+                    values: cos2,
+                    key: 'Thyphoid',
+                    color: '#B8FF2F',
+                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                },
+                {
+                    values: cos3,
+                    key: 'Diabeties',
+                    color: '#73CBA7',
+                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                }
+            ];
+        };
+
+        function genderData() {
+            var sin = [],
+                cos = [];
+
+            //Data is represented as an array of {x,y} pairs.
+            for (var i = 0; i < 7; i++) {
+                sin.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 100)
+                });
+                cos.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 100)
+                });
+            }
+
+            //Line chart data should be sent as an array of series objects.
+            return [{
+                    values: sin, //values - represents the array of {x,y} data points
+                    key: 'Female', //key  - the name of the series.
+                    color: '#DA80D1' //color - optional: choose your own line color.
+                },
+                {
+                    values: cos,
+                    key: 'Male',
+                    color: '#67B0C3'
+                }
+            ];
+        };
+
+        function ageData() {
+            var sin = [],
+                sin2 = [],
+                cos = [],
+                cos2 = [],
+                cos3 = [];
+
+            //Data is represented as an array of {x,y} pairs.
+            for (var i = 0; i < 7; i++) {
+                sin.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                sin2.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                cos.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                cos2.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+                cos3.push({
+                    x: i,
+                    y: Math.floor(Math.random() * 1000)
+                });
+            }
+
+            //Line chart data should be sent as an array of series objects.
+            return [{
+                    values: sin, //values - represents the array of {x,y} data points
+                    key: '0-10', //key  - the name of the series.
+                    color: '#DA80D1' //color - optional: choose your own line color.
+                },
+                {
+                    values: cos,
+                    key: '10-25',
+                    color: '#67B0C3'
+                },
+                {
+                    values: sin2,
+                    key: '25-50',
+                    color: '#FFD876',
+                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                },
+                {
+                    values: cos2,
+                    key: '50-50',
+                    color: '#B8FF2F',
+                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                },
+                {
+                    values: cos3,
+                    key: '70 and Above',
+                    color: '#73CBA7',
+                    //area: true      //area - set to true if you want this line to turn into a filled area chart.
+                }
+            ];
+        };
+
     }
 })();
